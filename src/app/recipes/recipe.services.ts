298020be6a7983f3adf9d2465ac73dda4d6ccd64
Patricio Monpelat <patricio.monpelat@gmail.com>
2021-07-28 20:39:41 -0300

    new recipe added to list

--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -2,8 +2,9 @@ import { Recipe } from './recipe.model';
 
 export class RecipeService {
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe 1', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-        new Recipe('A Test Recipe 2', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+        new Recipe('Recipe 1', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+        new Recipe('Recipe 2', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe('Recipe 3', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
       ];
 
       getRecipes() {
