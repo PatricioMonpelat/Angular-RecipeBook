f782635578f896881c13667121baf38b812df1db
Patricio Monpelat <patricio.monpelat@gmail.com>
2021-08-11 22:04:55 -0300

    recipe service

--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -1,14 +1,50 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Recipe 1', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-        new Recipe('Recipe 2', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Recipe 3', 'This is the intructions for the recipe', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        // tslint:disable-next-line: max-line-length
+        new Recipe(
+            'Big Tasty',
+             'This is the intructions for the Big Tasty recipe',
+              'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kQXPkDxA/200/200/original?country=ar',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Bread', 2),
+                new Ingredient('Letuce', 1),
+                new Ingredient('Big Tasty Salse', 1),
+                new Ingredient('French Fries', 1)
+            ]),
+        new Recipe(
+            'Grand Doble McBacon',
+             'This is the intructions for the Doble Cuarto de Libra recipe',
+              'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kQXnpmyG/200/200/original?country=ar',
+              [
+                new Ingredient('Meat', 2),
+                new Ingredient('Bread', 2),
+                new Ingredient('Cheddar', 2),
+                new Ingredient('Ketchup', 1),
+                new Ingredient('Mostard', 1),
+                new Ingredient('Onion', 1),
+                new Ingredient('French Fries', 1)
+              ]),
+        new Recipe('Grand Triple McBacon',
+         'This is the intructions for the Big Mc recipe',
+          'https://cache-backend-mcd.mcdonaldscupones.com/media/image/product$kQX3BMhy/200/200/original?country=ar',
+          [
+            new Ingredient('Meat', 2),
+            new Ingredient('Bread', 3),
+            new Ingredient('Cheddar', 2),
+            new Ingredient('Big Mc Special Salse', 1),
+            new Ingredient('Onion', 1),
+            new Ingredient('Letuce', 1),
+            new Ingredient('Pepino', 1),
+            new Ingredient('French Fries', 1)
+          ])
       ];
 
       getRecipes() {
