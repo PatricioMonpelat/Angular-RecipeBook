3a488935696b944c19ffc0f3af2e2ff14e870aa0
Patricio Monpelat <patricio.monpelat@gmail.com>
2021-08-24 16:03:25 -0300

    adding editing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -8,8 +9,10 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 const appRoutes: Routes = [
     { path: '' , redirectTo: '/recipes', pathMatch: 'full'},
     { path: 'recipes' , component: RecipesComponent, children:[
-        { path: '',component: RecipesStartComponent},
-        { path: ':id', component: RecipesDetailComponent}
+        { path: '',component: RecipesStartComponent },
+        { path: 'new', component: RecipesEditComponent },
+        { path: ':id', component: RecipesDetailComponent },
+        { path: ':id/edit', component: RecipesEditComponent }
     ] },
     { path: 'shopping-list' , component: ShoppingListComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.com
     ShoppingListComponent,
     ShoppingEditComponent,
     DropdownDirective,
-    RecipesStartComponent
+    RecipesStartComponent,
+    RecipesEditComponent
   ],
   imports: [
     BrowserModule,
