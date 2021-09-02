1752360057de45bbf6645c8ae5c28513621ae5bf
Patricio Monpelat <patricio.monpelat@gmail.com>
2021-09-02 16:23:10 -0300

    fetching recipes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,4 +15,8 @@ export class HeaderComponent {
     onSaveData() {
         this.dataStorageService.storeRecipes();
     }
+
+    onFetchData() {
+        this.dataStorageService.fetchRecipes();
+    }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.services';
-
+import { Recipe } from '../recipes/recipe.model';
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
     constructor( private http: HttpClient, private recipeService: RecipeService) {}
@@ -13,5 +13,14 @@ export class DataStorageService {
          .subscribe(response => {
              console.log(response);
             });
+    }
+
+    fetchRecipes() {
+        this.http
+        .get<Recipe[]>('https://courseprojectangular-dde40-default-rtdb.firebaseio.com/recipes.json')
+        .subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+            console.log(recipes);
+        });
     }
 }
