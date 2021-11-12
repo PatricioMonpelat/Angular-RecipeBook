ef20e0fec84eff75cdbe7a7657104f45c5db5c93
Patricio Monpelat <patricio.monpelat@gmail.com>
2021-11-12 14:41:17 -0300

    auto login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -20,7 +20,7 @@ export class AuthService {
     user = new BehaviorSubject<User>(null);
 
 
-    constructor(private http: HttpClient , private router: Router) { }
+    constructor(private http: HttpClient, private router: Router) { }
 
     signup(email: string, password: string) {
         return this.http
@@ -67,6 +67,25 @@ export class AuthService {
                 })
             );
     }
+
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if (!userData) {
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -83,6 +102,7 @@ export class AuthService {
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
